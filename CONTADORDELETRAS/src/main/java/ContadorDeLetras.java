2a0652ba70deff8cf2d8103fd1ac683cf6902624
Mayara <mayaracauny@gmail.com>
2024-04-17 14:22:00 -0300

    Update ContadorDeLetras.java

--- CONTADORDELETRAS/src/main/java/ContadorDeLetras.java
+++ CONTADORDELETRAS/src/main/java/ContadorDeLetras.java
@@ -5,19 +5,16 @@ public class ContadorDeLetras {
 		
 		String texto = "Eu sou a pessoa mais incrível que eu posso ser, e através do meu esforço vou conquistar o que já é MEU";
 		char a = 'a';
-		char acento = 'á';
 		int quantidade = 0;
-		int valor = 0;
+		
 		for (int i=0; i<texto.length(); i++) {
 			  char c = texto.charAt(i);
 			if (c == a) {
 				quantidade++;
-			}if (c == acento) {
-				valor++;
 			}
 		}
 		
-		System.out.println("A letra 'a' aparece " + quantidade + " vezes sem acento e " + valor + " vez com acento na mensagem.");
+		System.out.println("A letra 'a' aparece " + quantidade +  " vezes na mensagem.");
 	}
 
 		
